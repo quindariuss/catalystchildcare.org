f0306e442b81d8c072a97ecb33828bb59e99832c
Al <dandavisjs@protonmail.com>
2022-07-01 13:40:03 -0700

    Favicon, changes png shapes to svg, nav menu css tweak

--- scripts/shapes.js
+++ scripts/shapes.js
@@ -1,6 +1,5 @@
 // array of shape image file names (assets/images/shapes)
-const about = ['pentagon', 'circle', 'triangle'];
-
+const about = ['pentagon', 'circle', 'square'];
 
 // Looping twice (two sections: head, about) - sections we need to place shapes to
 for (let x = 0; x < 2; x++) {
@@ -9,12 +8,12 @@ for (let x = 0; x < 2; x++) {
 		// Looping to generate several shapes of the same form
 		for (var i = 0; i < getRandomNumber(2, 3); i++) {
 			var shape = document.createElement('img');
-			shape.className = 'bg-shape'
+			shape.className = 'bg-shape';
 			// IF x == 0 We're in Head section 
 			// ELSE We're in About section
-			shape.src = `assets/images/shapes/${!x ? shapeImg : shapeImg+'-grey'}.png`;
-			let section = !x ? 'head' : 'about'
-			document.getElementById(section).appendChild(shape)
+			shape.src = `assets/images/shapes/${!x ? shapeImg : shapeImg+'-grey'}.svg`;
+			let section = !x ? 'head' : 'about';
+			document.getElementById(section).appendChild(shape);
 
 
 			// get the dimensions of section
@@ -24,8 +23,8 @@ for (let x = 0; x < 2; x++) {
 			// create random Number
 			randomTop = getRandomNumber(0, section==="head"?sectionHeight-150:sectionHeight);
 			randomLeft = getRandomNumber(0, sectionWidth);
-			randomSize = getRandomNumber(30, 80)
-			randomRotate = getRandomNumber(0, 360)
+			randomSize = getRandomNumber(30, 80);
+			randomRotate = getRandomNumber(0, 360);
 			// update style properties
 			shape.style.top = randomTop + "px";
 			shape.style.left = randomLeft + "px";
@@ -38,6 +37,5 @@ for (let x = 0; x < 2; x++) {
 
 // function that returns a random number between a min and max
 function getRandomNumber(min, max) {
-
 	return Math.random() * (max - min) + min;
-}
+};
